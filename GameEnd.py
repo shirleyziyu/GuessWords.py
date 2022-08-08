ca3496dd00cda07b93818927402820631ffa267c
shirleyziyu <2569816038@qq.com>
2022-08-08 22:12:11 +0800

    一次失败的尝试

--- GameEnd.py
+++ GameEnd.py
@@ -7,8 +7,9 @@ class gameEnd(GameResult.gameResult):
     def __init__(self, parent):
         GameResult.gameResult.__init__(self, parent)
 
-    def openFrame(self):
+    def openFrame(self, para):
         app = wx.App(False)
         frame = gameEnd(None)
+        self.gameResult.SetValue(para)
         frame.Show(True)
         app.MainLoop()
